cbb5b930f126567ad374e63d9cc049765143c1da
Remix Workspace <>
2022-10-28 22:37:03 +0200

    *

--- erc20/ERC20.sol
+++ erc20/ERC20.sol
@@ -7,8 +7,8 @@ contract ERC20 is IERC20 {
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
-    string public name = "Solidity by Example";
-    string public symbol = "SOLBYEX";
+    string public name = "POLKACAT";
+    string public symbol = "PLKT";
     uint8 public decimals = 18;
 
     function transfer(address recipient, uint amount) external returns (bool) {
